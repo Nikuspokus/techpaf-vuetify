2bdae9b875da7c967ad2c07db82d50b1ba8d87d8
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-19 14:21:02 +0200

    techpaf-vuetify - WIP

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      absolute
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-app-bar-nav-icon>sdfsdfsdf</v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+<v-spacer></v-spacer>
+      <v-divider vertical ></v-divider>
+
+      <v-btn icon>
+        <v-icon>mdi-bell-outline</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-sheet
+    >
+      <v-container style="height: 60px;"> </v-container>
+    </v-sheet>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
